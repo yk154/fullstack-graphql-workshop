ecda5e138c69b9b70002393c26b7ecb11249968e
Amy <yeonwoo@gim-yeon-us-MacBook-Pro.local>
2018-07-05 09:13:29 +0100

    pokemon

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { GraphQLServer } from 'graphql-yoga'
 import songs from "./data/songs"
+import playlists from "./data/playlists"
+import users from "./data/users"
+import fetch from "node-fetch"
+import { raw } from 'body-parser';
 
 const resolvers = {
   Query: {
@@ -7,10 +11,53 @@ const resolvers = {
       if(args.first) {
         return songs.slice(0, args.first)
       }
+      if(args.last){
+        return songs.slice(songs.length - args.last, songs.length)
+      }
       return songs;
     },
- 
+		playlists(parent, args, ctx, info) {
+			if(args.first) {
+        return playlists.slice(0, args.first)
+      }
+      if(args.last){
+        return playlists.slice(playlists.length - args.last, playlists.length)
+      }
+			return playlists;
+		},
+		users(parent, args, ctx, info) {
+			if(args.first) {
+        return users.slice(0, args.first)
+      }
+      if(args.last){
+        return users.slice(users.length - args.last, users.length)
+      }
+      // for(var i = 0; i <users.length; i++){
+      //   if(args.email == users[i].email){
+      //     return users[i].email
+      //   }
+      // }
+      if(args.email){
+        return [users.find (
+          function(user) {
+            return user.email == args.email
+          }
+        )]
+      }
+			return users;
+    },
 
+    pokemon(parent, args, ctx, info){
+      if(args.name){
+        fetch('https://pokeapi.co/api/v2/pokemon/squirtle')
+        .then((raw) => raw.json())
+        .then(console.log)
+      }
+    },
+
+    randomSongs(parent, args, ctx, info) {
+      return songs[Math.floor(Math.random() * songs.length)];
+    }
   }
 }
 
